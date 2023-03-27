1e870880b4b38ba9a47e6fe310cf6ab0bd7d27e2
thinh2507 <117496244+thinh250702@users.noreply.github.com>
2023-03-27 15:26:40 +0700

    thinh's update
    Add file menu_ktchen.php, checkout.php, order_list.php
    Modify file Header.php, Sidebar.php

---
+++ A/Modules/Header.php
@@ -0,0 +1,5 @@
+<div class="header">
+    <i class="fa-solid fa-user"></i>
+    <p>Nguyen Truong Thinh</p>
+    <a href=""><i class="fa-solid fa-right-from-bracket"></i></a>
+</div>
---
+++ A/views/session/checkout.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Checkout</title>
+    <!-- Poppin font family -->
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <!-- Font Awesone for Icon -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <!-- CSS file from user -->
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/shadow.css">
+</head>
+<body>
+    <div class="container invoice">
+        <div><a class="nav_btn" href="">Back</a></div>
+        <div class="invoice_wrapper shadow-4">
+            <div class="inv_header">
+                <img src="./img/logo.png" alt="">
+                <div class="divide"></div>
+                <p>Invoice</p>
+                <img src="./img/invoice_deco_cropped.png" alt="">
+            </div>
+            <div class="inv_body">
+                <div class="invoice_info">
+                    <div>
+                        <p>TABLE: <b>T-01</b></p>
+                        <p>PAX: <b>04</b></p>
+                        <p>ARRIVE TIME: <b>20:30</b></p>
+                    </div>
+                    <div class="wide"></div>
+                    <div>
+                        <p>INVOICE ID: <b>A1234</b></p>
+                        <p>DATE: <b>20/03/2022</b></p>
+                    </div>
+                </div>
+                <div class="prod_list">
+                    <div class="prod_row head">
+                        <span>No.</span>
+                        <span>Item Description</span>
+                        <span>Price</span>
+                        <span>Qty</span>
+                        <span>Total</span>
+                    </div>
+                    <div class="prod_row odd">
+                        <span>1</span>
+                        <span>Burger</span>
+                        <span>20.000</span>
+                        <span>5</span>
+                        <span>100.000</span>
+                    </div>
+                    <div class="prod_row even">
+                        <span>2</span>
+                        <span>Burger</span>
+                        <span>20.000</span>
+                        <span>5</span>
+                        <span>100.000</span>
+                    </div>
+                    <div class="prod_row odd">
+                        <span>1</span>
+                        <span>Burger</span>
+                        <span>20.000</span>
+                        <span>5</span>
+                        <span>100.000</span>
+                    </div>
+                    <div class="prod_row even">
+                        <span>2</span>
+                        <span>Burger</span>
+                        <span>20.000</span>
+                        <span>5</span>
+                        <span>100.000</span>
+                    </div>
+                </div>
+                <div class="invoice_info">
+                    <div>
+                        <p>Thank you for your visit!</p>
+                        <div>
+                            <p>Payment Info:</p>
+                            <p>Account Number: <b>123456789</b></p>
+                            <p>Account Name: <b>NGUYEN TRUONG THINH</b></p>
+                            <p>Bank Name: <b>Vietinbank</b></p>
+                        </div>
+                    </div>
+                    <div class="wide"></div>
+                    <div>
+                        <p>SUB TOTAL: <b>120.000</b></p>
+                        <p>TAX: <b>10.00%</b></p>
+                        <p>TOTAL: <b>150.000</b></p>
+                    </div>
+                </div>
+            </div>
+            <form action="">
+                <div class="pay_section">
+                    <div>
+                        <div>
+                            <label for="pay_id">Pay ID</label>
+                            <input type="text" name="pay_id" id="">
+                        </div>
+                        <div>
+                            <label for="pay_id">Pay Amount</label>
+                            <input type="text" name="pay_id" id="">
+                        </div>
+                        <div>
+                            <label for="pay_id">Pay Method</label>
+                            <select name="pay_method" id="">
+                                <option value="cash">Cash</option>
+                                <option value="bank">Banking</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <input type="submit" value="Pay">
+            </form>
+        </div>
+        
+    </div>
+</body>
+</html>
