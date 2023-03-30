b2e68f0190a91fb3353eb2b24312ed8a2802bab8
thinh2507 <117496244+thinh250702@users.noreply.github.com>
2023-03-30 08:25:39 +0700

    Add menu sidebar in session.menu.php

--- A/views/session/menu.php
+++ A/views/session/menu.php
@@ -7,6 +7,22 @@ $dish = dish::all();
 ?>
 
 <div class="container">
+    <div class="menu_sidebar">
+        <div class="logo">
+            <img src="./img/logo.png" alt="">
+        </div>
+        <h1>MENU</h1>
+        <div id="category">
+            <?php
+                foreach ($dish as $item) {
+            ?>
+            <a class="dish_cate" href=""><?=$item -> dish_name?></a>
+            <!-- <button class="active">Fried chicken</button> -->
+            <?php
+                   }
+                ?>
+        </div>
+    </div>
     <div class="main">
         <!-- <div class="order_detail shadow-4">
                     <form action="">
